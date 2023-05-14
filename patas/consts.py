54c6c130c5f1f2f2237466db26e2559eb2f35f23
Diego Souza <diegofpsouza@gmail.com>
2023-05-14 11:02:40 -0300

    Bugfix in README.md

--- patas/consts.py
+++ patas/consts.py
@@ -1,7 +1,7 @@
 name = "patas"
-version = "2.0.4"
+version = "2.0.5"
 author = "Diego Souza"
 author_email = "diegofpsouza+patas@gmail.com"
 url = "https://github.com/diegofps/patas"
-description = "Parallelize any program and aggregate results from stdout."
+description = "Hyperparameter search with a single command"
 
